36a9332f8d0763d2397fda79d815c325f2a0efa1
vasigavr1 <vasigavr1@gmail.com>
2020-07-08 13:45:48 +0100

    git piush scripts

--- bin/git-scripts/git-all-push.sh
+++ bin/git-scripts/git-all-push.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-/home/s1687259/odyssey/bin/git-odyssey-push.sh "$1"
-/home/s1687259/odyssey/bin/git-kite-push.sh "$1"
-/home/s1687259/odyssey/bin/git-zk-push.sh "$1"
-/home/s1687259/odyssey/bin/git-shared-push.sh "$1"
+/home/s1687259/odyssey/bin/git-scripts/git-odyssey-push.sh "$1"
+/home/s1687259/odyssey/bin/git-scripts/git-kite-push.sh "$1"
+/home/s1687259/odyssey/bin/git-scripts/git-zk-push.sh "$1"
+/home/s1687259/odyssey/bin/git-scripts/git-shared-push.sh "$1"
