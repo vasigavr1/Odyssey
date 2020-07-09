43748c3fc541a8dbd1a43231d6a067fd2738c94a
vasigavr1 <vasigavr1@gmail.com>
2020-07-09 21:13:14 +0100

    Unstable: standarizing multicast checkpoint

--- bin/copy-executables.sh
+++ bin/copy-executables.sh
@@ -22,7 +22,7 @@ SCRIPT_FOLDER=$4
 DEST_FOLDER=$5
 
 cd $MAKE_FOLDER
-make clean
+#make clean
 make $1
 rm -rf core
 cd -
--- bin/git-scripts/git-all-push.sh
+++ bin/git-scripts/git-all-push.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 
+
+echo "----------Pushing to Odyssey-------------------------"
 /home/s1687259/odyssey/bin/git-scripts/git-odyssey-push.sh "$1"
+echo "------------------------------------------------------"
+
+echo "----------Pushing to Kite-------------------------"
 /home/s1687259/odyssey/bin/git-scripts/git-kite-push.sh "$1"
+echo "------------------------------------------------------"
+
+echo "----------Pushing to Zookeeper-------------------------"
 /home/s1687259/odyssey/bin/git-scripts/git-zk-push.sh "$1"
+echo "------------------------------------------------------"
+
+echo "----------Pushing to Shared-------------------------"
 /home/s1687259/odyssey/bin/git-scripts/git-shared-push.sh "$1"
+echo "------------------------------------------------------"
