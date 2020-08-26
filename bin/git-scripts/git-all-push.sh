ac4065d78d9836538c49a5f90566d82276574430
vasigavr1 <vasigavr1@gmail.com>
2020-08-26 20:11:37 +0100

    derecho minor change

--- bin/git-scripts/git-all-push.sh
+++ bin/git-scripts/git-all-push.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
 
-echo "----------Pushing to Odyssey-------------------------"
-/home/s1687259/odyssey/bin/git-scripts/git-odyssey-push.sh "$1"
-echo "------------------------------------------------------"
-
 echo "----------Pushing to Kite-------------------------"
 /home/s1687259/odyssey/bin/git-scripts/git-kite-push.sh "$1"
 echo "------------------------------------------------------"
@@ -15,4 +11,8 @@ echo "------------------------------------------------------"
 
 echo "----------Pushing to Od-lib-------------------------"
 /home/s1687259/odyssey/bin/git-scripts/git-odlib-push.sh "$1"
+echo "------------------------------------------------------"
+
+echo "----------Pushing to Odyssey-------------------------"
+/home/s1687259/odyssey/bin/git-scripts/git-odyssey-push.sh "$1"
 echo "------------------------------------------------------"
