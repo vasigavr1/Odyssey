cf3666066e9f99931d05f939af8d4b1b0e99b0c2
vasigavr1 <vasigavr1@gmail.com>
2020-08-28 17:09:45 +0100

    kite

--- bin/git-scripts/git-all-push.sh
+++ bin/git-scripts/git-all-push.sh
@@ -2,17 +2,17 @@
 
 
 echo "----------Pushing to Kite-------------------------"
-/home/s1687259/odyssey/bin/git-scripts/git-kite-push.sh "$1"
+/home/s1687259/odyssey/bin/git-scripts/git-kite-push.sh "kite" "$1"
 echo "------------------------------------------------------"
 
 echo "----------Pushing to Zookeeper-------------------------"
-/home/s1687259/odyssey/bin/git-scripts/git-zk-push.sh "$1"
+/home/s1687259/odyssey/bin/git-scripts/git-zk-push.sh "zookeeper" "$1"
 echo "------------------------------------------------------"
 
 echo "----------Pushing to Od-lib-------------------------"
-/home/s1687259/odyssey/bin/git-scripts/git-odlib-push.sh "$1"
+/home/s1687259/odyssey/bin/git-scripts/git-odlib-push.sh "odlib" "$1"
 echo "------------------------------------------------------"
 
 echo "----------Pushing to Odyssey-------------------------"
-/home/s1687259/odyssey/bin/git-scripts/git-odyssey-push.sh "$1"
+/home/s1687259/odyssey/bin/git-scripts/git-odyssey-push.sh "" "$1"
 echo "------------------------------------------------------"
