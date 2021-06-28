65ad3756cdc105d83913ac45d8f55a9010de4b5f
vasilis gavrielatos <vasigavr1@gmail.com>
2021-06-28 15:59:29 +0100

    init-submodule script

--- bin/git-scripts/init_submodules.sh
+++ bin/git-scripts/init_submodules.sh
@@ -2,4 +2,5 @@
 
 GIT_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 cd "$GIT_SCRIPT_DIR/../.."
+git submodule update --init
 git submodule foreach --recursive git checkout master
